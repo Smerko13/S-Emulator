17dcd0794f4923eb5c266213e2d97bf3fa614287
Daniel Smerkovich <danielsmerko13@gmail.com>
2025-10-10 11:46:33 +0300

    load file button opens and explorer

--- S-Emulator-Client/src/components/mainDashboard/header/HeaderController.java
+++ S-Emulator-Client/src/components/mainDashboard/header/HeaderController.java
@@ -1,10 +1,15 @@
 package components.mainDashboard.header;
 
 import components.mainDashboard.clientMainController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class HeaderController {
     @FXML private TextField creditInputTextField;
@@ -21,5 +26,29 @@ public class HeaderController {
 
     public void updateUserName(String userName) {
         userNameLabel.setText("User Name: " + userName);
+    }
+
+    public void loadFileButtonPressed(ActionEvent actionEvent) {
+        // Create a FileChooser
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select XML File");
+
+        // Set extension filter for XML files
+        FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
+        fileChooser.getExtensionFilters().add(xmlFilter);
+
+        // Set initial directory to user's documents folder
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        // Get the current stage from the button
+        Stage stage = (Stage) loadFileButton.getScene().getWindow();
+
+        // Show the file chooser dialog
+        File selectedFile = fileChooser.showOpenDialog(stage);
+
+        // If a file was selected, update the file path text field
+        if (selectedFile != null) {
+            filePathTextField.setText(selectedFile.getAbsolutePath());
+        }
     }
 }
